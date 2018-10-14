2530d63d68384aa0ddc67a57022f055e217ea156
musanss <39636638+musanss@users.noreply.github.com>
2018-10-15 00:20:53 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app, {transports:[ 'websocket', 'htmlfile', 'xhr-polling', 'jsonp-polling']})
+  , io = require('socket.io').listen(app)
   , fs = require('fs')
 
 app.listen(8085);
