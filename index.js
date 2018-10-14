f0cb7ac09c551825dba7924ab2c96d4a4987819b
musanss <39636638+musanss@users.noreply.github.com>
2018-10-15 00:04:38 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-var server = require('http').createServer();
+/*var server = require('http').createServer();
 var io = require('socket.io')(server);
 io.on('connection', function(client){
   
 });
-server.listen(3000);
+server.listen(3000);*/
 
-/*var app = require('express')();
+var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-io.on('connection', function(){
+io.configure(function () {  
+  io.set("transports", ["flashsocket" , "websocket", "xhr-polling", "htmlfile", "jsonp-polling"]);
+  io.set("polling duration", 10); 
+  });
+
+io.on('connection', function(socket){
 	
 });
-server.listen(3000);*/
+server.listen(3000);
