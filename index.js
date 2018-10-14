fde18f1852a7402946d93d950e09c39eb82a9f90
musanss <39636638+musanss@users.noreply.github.com>
2018-10-15 00:14:45 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,70 @@
-/*var server = require('http').createServer();
-var io = require('socket.io')(server);
-io.on('connection', function(client){
-  
-});
-server.listen(3000);*/
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app, {transports:['flashsocket', 'websocket', 'htmlfile', 'xhr-polling', 'jsonp-polling']})
+  , fs = require('fs')
 
-var app = require('express')();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-io.configure(function () {  
-  io.set("transports", ["flashsocket" , "websocket", "xhr-polling", "htmlfile", "jsonp-polling"]);
-  io.set("polling duration", 10); 
+app.listen(8085);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+
+io.sockets.on('connection', function (socket) {
+  //emits an object {objectdata}
+  var roomname;
+  var theData; 
+
+  socket.emit('WelcomeMsg', 'hello world');
+
+
+  socket.on('createRoom', function(){ 
+    roomname = makeid();
+    socket.room = roomname;
+    socket.join(roomname);
+    socket.emit("createRoomResponse", socket.room);
+    console.log(roomname);
   });
 
-io.on('connection', function(socket){
-	
+  socket.on('getRoom', function(roomname){
+    console.log('getRoom');
+    socket.emit('getRoomResponse', roomname)
+  });
+  
+  socket.on('joinRoom', function(roomname){
+    socket.room = roomname;
+    socket.join(roomname);
+    io.sockets.in(roomname).emit('joinRoomResponse', socket.room);
+    console.log(roomname);
+  });
+
+  socket.on('helloRoom', function(roomname){
+    console.log('helloRoom');
+    io.sockets.in(roomname).emit('helloRoomResponse', 'hello room');
+  });
+
+  socket.on('sendData', function(theData){
+    console.log('sendData', theData);
+    io.sockets.in(roomname).emit('sendDataResponse', theData);
+  });
+  
 });
-server.listen(3000);
+
+function makeid(){
+    var text = "";
+    //no 0 or O;
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    
+    return text;
+}
