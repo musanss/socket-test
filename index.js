3b108275174bccb3b7c7994da08bd08486dcf88c
musanss <39636638+musanss@users.noreply.github.com>
2018-10-14 23:35:28 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ var server = new http.createServer();
 server.listen(3000);
 var io = require('socket.io').listen(server, {transports: ["flashsocket" ,"websocket", "xhr-polling", "htmlfile", "jsonp-polling"]});
 
+io.sockets.on('connection', function (socket) {
+   socket.emit('WelcomeMsg', 'hello world');
+   });
